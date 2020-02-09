6fc954f64182d09e99a46a9165c941836d90a2e1
RockinBach <60404675+RockinBach@users.noreply.github.com>
2020-02-09 20:10:20 +1100

    We got this

--- index.js
+++ index.js
@@ -262,16 +262,8 @@ app.post('/move', (request, response) => {
       // Take the first location off the queue
       var currentLocation = queue.shift();
 
-      // Explore up
-      var newLocation = exploreInDirection(currentLocation, 'up', gameMap);
-      if (newLocation.status === 3) {
-        return newLocation.path;
-      } else if (newLocation.status === 'Valid') {
-        queue.push(newLocation);
-      }
-
-      // Explore right
-      var newLocation = exploreInDirection(currentLocation, 'right', gameMap);
+      // Explore left
+      var newLocation = exploreInDirection(currentLocation, 'left', gameMap);
       if (newLocation.status === 3) {
         return newLocation.path;
       } else if (newLocation.status === 'Valid') {
@@ -286,8 +278,16 @@ app.post('/move', (request, response) => {
         queue.push(newLocation);
       }
 
-      // Explore left
-      var newLocation = exploreInDirection(currentLocation, 'left', gameMap);
+      // Explore right
+      var newLocation = exploreInDirection(currentLocation, 'right', gameMap);
+      if (newLocation.status === 3) {
+        return newLocation.path;
+      } else if (newLocation.status === 'Valid') {
+        queue.push(newLocation);
+      }
+
+      // Explore up
+      var newLocation = exploreInDirection(currentLocation, 'up', gameMap);
       if (newLocation.status === 3) {
         return newLocation.path;
       } else if (newLocation.status === 'Valid') {
@@ -336,13 +336,13 @@ app.post('/move', (request, response) => {
     var dft = currentLocation.distanceFromTop;
     var dfl = currentLocation.distanceFromLeft;
 
-    if (direction === 'up') {
+    if (direction === 'left') {
       dft -= 1;
-    } else if (direction === 'right') {
-      dfl += 1;
     } else if (direction === 'down') {
+      dfl += 1;
+    } else if (direction === 'right') {
       dft += 1;
-    } else if (direction === 'left') {
+    } else if (direction === 'up') {
       dfl -= 1;
     }
 
