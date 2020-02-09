7e2ffc2c33ea0b296b9325c4bd5a81eba4794d1d
RockinBach <60404675+RockinBach@users.noreply.github.com>
2020-02-09 19:18:39 +1100

    We got this

--- index.js
+++ index.js
@@ -75,41 +75,41 @@ app.post('/move', (request, response) => {
     prevDirection = 0;
   }
 
-    //find closest food     
-    for(let i = 0; i < foods.length; i++){
-      if( foods[0].x - mySnake[0].x < 0){
-        //check if going right to avoid collision then go left
-        if(prevDirection != 3){
-          d = 2;
-        }else{
-          d = 0;
-        }
-      //if food is above or below the snake
-      } else if(foods[0].x - mySnake[0].x == 0){
-        //food is above snake
-        if (foods[0].y - mySnake[0].y < 0){
-          if(prevDirection != 1){
-            d = 0;
-          }else{
-            d = 3;
-          }
-        //food is below snake
-        } else {
-          if(prevDirection != 0){
-            d = 1;
-          }else{
-            d = 3;
-          }
-        }
+  //find closest food     
+  for(let i = 0; i < foods.length; i++){
+    if( foods[0].x - mySnake[0].x < 0){
+      //check if going right to avoid collision then go left
+      if(prevDirection != 3){
+        d = 2;
       }else{
-        //food is right of snake
-        if(prevDirection != 2){
-          d = 3
-        }else{
+        d = 0;
+      }
+    //if food is above or below the snake
+    } else if(foods[0].x - mySnake[0].x == 0){
+      //food is above snake
+      if (foods[0].y - mySnake[0].y < 0){
+        if(prevDirection != 1){
           d = 0;
+        }else{
+          d = 3;
+        }
+      //food is below snake
+      } else {
+        if(prevDirection != 0){
+          d = 1;
+        }else{
+          d = 3;
         }
       }
+    }else{
+      //food is right of snake
+      if(prevDirection != 2){
+        d = 3
+      }else{
+        d = 0;
+      }
     }
+  }
 
   //check for collisions with itself
   for(let i = 4; i < mySnake.length; i++){
@@ -147,43 +147,43 @@ app.post('/move', (request, response) => {
      Collision[3] = 1;
   }
 
-    //Handle collisions
-    //check if any collisons
-    if(Collision[0] == 1 || Collision[1] == 1 || Collision[2] == 1 || Collision[3] == 1){
-      //check if going up and upper collision
-      if(prevDirection == 0 && Collision[0] == 1){
-        //Check if left is clear
-        if(Collision[2] == 0){
-          //turn left
-          d = 2;
-        } else {
-          //if not clear turn right
-          d = 3;
-        }
-      } else if(prevDirection == 1 && Collision[1] == 1){
-        if(Collision[2] == 0){
-          d = 2;
-        } else {
-          d = 3;
-        }
-      } else if(prevDirection == 2 && Collision[2] == 1){
-        if(Collision[0] == 0){
-          d = 0;
-        } else {
-          d = 1;
-        }
-      } else if(prevDirection == 3 && Collision[3] == 1){
-        if(Collision[0] == 0){
-          d = 0;
-        } else {
-          d = 1;
-        }
-      }else{
-      //d = prevDirection;
+  //Handle collisions
+  //check if any collisons
+  if(Collision[0] == 1 || Collision[1] == 1 || Collision[2] == 1 || Collision[3] == 1){
+    //check if going up and upper collision
+    if(prevDirection == 0 && Collision[0] == 1){
+      //Check if left is clear
+      if(Collision[2] == 0){
+        //turn left
+        d = 2;
+      } else {
+        //if not clear turn right
+        d = 3;
       }
-    } else {
-      //d = prevDirection;
+    } else if(prevDirection == 1 && Collision[1] == 1){
+      if(Collision[2] == 0){
+        d = 2;
+      } else {
+        d = 3;
+      }
+    } else if(prevDirection == 2 && Collision[2] == 1){
+      if(Collision[0] == 0){
+        d = 0;
+      } else {
+        d = 1;
+      }
+    } else if(prevDirection == 3 && Collision[3] == 1){
+      if(Collision[0] == 0){
+        d = 0;
+      } else {
+        d = 1;
+      }
+    }else{
+    //d = prevDirection;
     }
+  } else {
+    //d = prevDirection;
+  }
 
   console.log("Collisions " + Collision);
   console.log("previous Direction " + prevDirection + " move " + d);
@@ -195,49 +195,181 @@ app.post('/move', (request, response) => {
   // NEW SECTION THAT USES BREADTH FIRST SEARCH
   //
   // map all data into array
-    // create an array with enemy snake info
-    var enemySnakes = [];
-    for(let i = 0; i < request.body.board.snakes.length; i++){
-      if(request.body.board.snakes[i].name == myName){
-        console.log("its my snake");
-      }else{
-        for(let j = 0; j < request.body.board.snakes[i].body.length; j++){
-          const enemyPart = {x: request.body.board.snakes[i].body[j].x, y: request.body.board.snakes[i].body[j].y};
-          enemySnakes.push(enemyPart);
-        }
-      }  
-    }
+  // create an array with enemy snake info
+  var enemySnakes = [];
+  for(let i = 0; i < request.body.board.snakes.length; i++){
+    if(request.body.board.snakes[i].name == myName){
+      console.log("its my snake");
+    }else{
+      for(let j = 0; j < request.body.board.snakes[i].body.length; j++){
+        const enemyPart = {x: request.body.board.snakes[i].body[j].x, y: request.body.board.snakes[i].body[j].y};
+        enemySnakes.push(enemyPart);
+      }
+    }  
+  }
 
-    // creat a 2d array the height and width of the board rows and columns
-    var gameMap = [];
-    for(let i = 0; i < gameHeight; i++){
-      gameMap[i] = [];
-      for(let j = 0; j < gameWidth; j++){
-        gameMap[i][j] = 0;
+  // creat a 2d array the height and width of the board rows and columns
+  var gameMap = [];
+  for(let i = 0; i < gameHeight; i++){
+    gameMap[i] = [];
+    for(let j = 0; j < gameWidth; j++){
+      gameMap[i][j] = 0;
+    }
+  }
+
+  // put mySnake into the gameMap
+  for(let i = 0; i < mySnake.length; i++){
+    if(i == 0){
+      gameMap[mySnake[i].x][mySnake[i].y] = 1; //mysnake head = 1
+    }else{
+      gameMap[mySnake[i].x][mySnake[i].y] = 4; //mysnake body = 4
+    }
+  }
+  // put food locations into the gameMap
+  for(let i = 0; i < foods.length; i++){
+    gameMap[foods[i].x][foods[i].y] = 3; //food locations = 3 
+  }
+  // put enemy snake locations into the gameMap
+  for(let i = 0; i < enemySnakes.length; i++){
+    gameMap[enemySnakes[i].x][enemySnakes[i].y] = 4; //enemy snake locations = 4 
+  }
+
+  console.log("enemySnakes");
+  console.log(enemySnakes);
+  console.log(gameMap);
+
+  //This is an attmept at my maze solver
+  //
+  //
+  // Start location will be in the following format:
+    // [distanceFromTop, distanceFromLeft]
+  var findShortestPath = function(gameMap) {
+    var distanceFromTop = mySnake[0].y;
+    var distanceFromLeft = mySnake[0].x;
+    
+    gameMap[mySnake[0].x][mySnake[0].y] = 1;
+    // Each "location" will store its coordinates
+    // and the shortest path required to arrive there
+    var location = {
+      distanceFromTop: distanceFromTop,
+      distanceFromLeft: distanceFromLeft,
+      path: [],
+      status: 1
+    };
+
+    // Initialize the queue with the start location already inside
+    var queue = [location];
+
+    // Loop through the gameMap searching for the goal
+    while (queue.length > 0) {
+      // Take the first location off the queue
+      var currentLocation = queue.shift();
+
+      // Explore North
+      var newLocation = exploreInDirection(currentLocation, 'North', gameMap);
+      if (newLocation.status === 3) {
+        return newLocation.path;
+      } else if (newLocation.status === 'Valid') {
+        queue.push(newLocation);
+      }
+
+      // Explore East
+      var newLocation = exploreInDirection(currentLocation, 'East', gameMap);
+      if (newLocation.status === 3) {
+        return newLocation.path;
+      } else if (newLocation.status === 'Valid') {
+        queue.push(newLocation);
+      }
+
+      // Explore South
+      var newLocation = exploreInDirection(currentLocation, 'South', gameMap);
+      if (newLocation.status === 3) {
+        return newLocation.path;
+      } else if (newLocation.status === 'Valid') {
+        queue.push(newLocation);
+      }
+
+      // Explore West
+      var newLocation = exploreInDirection(currentLocation, 'West', gameMap);
+      if (newLocation.status === 3) {
+        return newLocation.path;
+      } else if (newLocation.status === 'Valid') {
+        queue.push(newLocation);
       }
     }
 
-    // put mySnake into the gameMap
-    for(let i = 0; i < mySnake.length; i++){
-      if(i == 0){
-        gameMap[mySnake[i].y][mySnake[i].x] = 1; //mysnake head = 1
-      }else{
-        gameMap[mySnake[i].y][mySnake[i].x] = 2; //mysnake body = 2
-      }
+    // No valid path found
+    return false;
+
+  };
+
+  // This function will check a location's status
+  // (a location is "valid" if it is on the gameMap, is not an "obstacle",
+  // and has not yet been visited by our algorithm)
+  // Returns "Valid", "Invalid", "Blocked", or "Goal"
+  var locationStatus = function(location, gameMap) {
+    var gridSize = gameMap.length;
+    var dft = location.distanceFromTop;
+    var dfl = location.distanceFromLeft;
+
+    if (location.distanceFromLeft < 0 ||
+        location.distanceFromLeft >= gridSize ||
+        location.distanceFromTop < 0 ||
+        location.distanceFromTop >= gridSize) {
+
+      // location is not on the gameMap--return false
+      return 'Invalid';
+    } else if (gameMap[dft][dfl] === 3) {
+      return 3;
+    } else if (gameMap[dft][dfl] !== 0) {
+      // location is either an obstacle or has been visited
+      return 'Blocked';
+    } else {
+      return 'Valid';
     }
-    // put food locations into the gameMap
-    for(let i = 0; i < foods.length; i++){
-      gameMap[foods[i].y][foods[i].x] = 3; //food locations = 3 
-    }
-    // put enemy snake locations into the gameMap
-    for(let i = 0; i < enemySnakes.length; i++){
-      gameMap[enemySnakes[i].y][enemySnakes[i].x] = 4; //enemy snake locations = 4 
+  };
+
+
+  // Explores the gameMap from the given location in the given
+  // direction
+  var exploreInDirection = function(currentLocation, direction, gameMap) {
+    var newPath = currentLocation.path.slice();
+    newPath.push(direction);
+
+    var dft = currentLocation.distanceFromTop;
+    var dfl = currentLocation.distanceFromLeft;
+
+    if (direction === 'North') {
+      dft -= 1;
+    } else if (direction === 'East') {
+      dfl += 1;
+    } else if (direction === 'South') {
+      dft += 1;
+    } else if (direction === 'West') {
+      dfl -= 1;
     }
 
-    console.log("enemySnakes");
-    console.log(enemySnakes);
-    console.log(gameMap);
+    var newLocation = {
+      distanceFromTop: dft,
+      distanceFromLeft: dfl,
+      path: newPath,
+      status: 'Unknown'
+    };
+    newLocation.status = locationStatus(newLocation, gameMap);
 
+    // If this new location is valid, mark it as 'Visited'
+    if (newLocation.status === 'Valid') {
+      gameMap[newLocation.distanceFromTop][newLocation.distanceFromLeft] = 'Visited';
+    }
+
+    return newLocation;
+  };
+
+
+  // OK. We have the functions we need--let's run them to get our shortest path!
+  console.log(findShortestPath(gameMap));
+  //
+  //
   // Response data
   var turn = arrMove[d];
   const data = {
