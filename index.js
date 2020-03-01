990893ad58414c6fb9ff4601a7bffd518671a3d9
RockinBach <60404675+RockinBach@users.noreply.github.com>
2020-03-01 16:57:41 +1100

    We got this

--- index.js
+++ index.js
@@ -196,24 +196,28 @@ app.post('/move', (request, response) => {
       
       if(mySnake[0].y > 0){
         console.log('up:');
+        console.log(gameMap[headX][headY - 1]);
         if(gameMap[headX][headY - 1].state == 'e' || gameMap[headX][headY - 1].state == 'su'){
           console.log('empty');
         }
       }
       if(mySnake[0].y < gameHeight - 1){
         console.log('down:');
+        console.log(gameMap[headX][headY + 1]);
         if(gameMap[headX][headY + 1].state == 'e' || gameMap[headX][headY - 1].state == 'sd'){
           console.log('empty');
         }
       }
       if(mySnake[0].x > 0){
         console.log('left:');
+        console.log(gameMap[headX - 1][headY]);
         if(gameMap[headX - 1][headY].state == 'e' || gameMap[headX][headY - 1].state == 'sl'){
           console.log('empty');
         }
       }
       if(mySnake[0].x < gameWidth - 1){
         console.log('right:');
+        console.log(gameMap[headX + 1][headY]);
         if(gameMap[headX + 1][headY].state == 'e' || gameMap[headX][headY - 1].state == 'sr'){
           console.log('empty');
         }
